4ee234539a677adea9be5eeb716aac1e8850a487
dddd-coder <82556034+dddd-coder@users.noreply.github.com>
2021-04-30 16:45:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var mario;
+var platformGroup;
+
+function preload()
+{}
+
+function setup() {
+  createCanvas(displayWidth, 668);
+  var countDistanceX = 0;
+  var platform;
+  var gap;
+
+  mario = new Player();
+  platformGroup= createGroup();
+  for (var i=0;i<14;i++)
+	 {
+      platform = new Platform(countDistanceX);
+      platformGroup.add(platform.spt);
+      gap=random([0,0,0,0,80]);
+      countDistanceX = countDistanceX + platform.rw + gap; 
+   }
+ 
+}
+
+function draw() {
+  background('skyblue');  
+  mario.applyGravity();
+  mario.spt.collide(platformGroup);
+
+  if(keyDown("right")){
+    mario.moveRight()
+  }
+translate( -mario.spt.x+width/2,0)
+  if(keyDown("left")){
+    mario.moveLeft()
+  }
+
+  if(keyDown("up")){
+    mario.moveUp()
+  }
+  drawSprites();
+}
+
+
+
